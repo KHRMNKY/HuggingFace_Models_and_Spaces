a7b3ab15bbd8cd5ddad8b64c52217f89df7255be
KAHRAMAN KAYA <khrmn8375@gmail.com>
2024-02-28 15:41:56 +0300

    Update app.py

--- Youtube_Transcript_Summarizer/app.py
+++ Youtube_Transcript_Summarizer/app.py
@@ -1,59 +1,112 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from openai import OpenAI
-import gradio as gr
-import os
-from dotenv import load_dotenv
+import argparse
 
-load_dotenv()
 
-api_key= os.getenv("api_key")
+def parse_args():
+  
+  """
+  A function to parse command-line arguments for the API key, model, and URL.
+  Returns the parsed arguments.
+
+  Returns:
+  argparse.Namespace: The parsed arguments.
+  """
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    dest = "api_key",
+    type = str,
+    help = "OpenAI api key or Deepseek coder api key"
+  )
+
+  parser.add_argument(
+    dest = "model",
+    type = str,
+    help = "OpenAI model to use for summarization"
+  )
+  
+  parser.add_argument(
+    dest = "url",
+    type = str,
+    help = "YouTube video URL"
+  )
+
+  return parser.parse_args()
+
+
+args = parse_args()
+api_key = args.api_key
+url = args.url
+model = args.model
+
 
 def youtube_url(url):
-    
-    """
-    Function to retrieve the transcript of a YouTube video based on the provided URL.
-    
-    Parameters:
-    url (str): The URL of the YouTube video.
-    
-    Returns:
-    str: The transcript of the video.
-    """
+  
+  """
+  Function to retrieve the transcript of a YouTube video based on the provided URL.
+  
+  Parameters:
+  url (str): The URL of the YouTube video.
+  
+  Returns:
+  str: The transcript of the video.
+  """
 
-    transcript = ""
-    list=url.split("=")
-    video_id = list[1]
-    list = YouTubeTranscriptApi.get_transcript(video_id, languages=['tr', 'en',"de"])
-    for dict in list:
-        transcript += dict["text"] + "\n"
-    return transcript
+  transcript = ""
+  list=url.split("=")
+  video_id = list[1]
+  list = YouTubeTranscriptApi.get_transcript(video_id, languages=['tr', 'en',"de"])
+  for dict in list:
+      transcript += dict["text"] + "\n"
+  return transcript
 
 
-def chat(prompt):
+
+def summarization(prompt):
 
   """
   This function takes a prompt as input and uses the OpenAI API to generate a chat completion based on the prompt. It returns the summary of the chat completion.
   """
 
   system_msg = "you are a youtube transcript summarizer."
+  
+  if model == "deepseek-chat":
+    
+    client = OpenAI(api_key = api_key, base_url="https://api.deepseek.com/v1")
+    completion = client.chat.completions.create(
+      model = model,
+      messages=[
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": prompt}
+      ]
+    )
 
-  client = OpenAI(api_key = api_key, base_url="https://api.deepseek.com/v1")
+    summary = completion.choices[0].message.content
+    return summary
+  
+  
+  else:
+    client = OpenAI(api_key = api_key)
 
-  completion = client.chat.completions.create(
-  model="deepseek-chat",
-  messages=[
-      {"role": "system", "content": system_msg},
-      {"role": "user", "content": prompt}
-  ]
-  )
-  summary = completion.choices[0].message.content
-  return summary
+    completion = client.chat.completions.create(
+      model = model,
+      messages=[
+        {"role": "system", "content": system_msg},
+        {"role": "user", "content": prompt}
+      ]
+    )
+
+    summary = completion.choices[0].message.content
+    return summary
+
 
 
 def main(url):
 
   """
-  Function to summarize the transcript of a YouTube video using the provided URL.
+  Function to
+  summarize the transcript of a YouTube video using the provided URL.
   
   Parameters:
   url (str): The URL of the YouTube video.
@@ -61,20 +114,12 @@ def main(url):
   Returns:
   str: The summary of the video transcript.
   """
-  
+
   transcript = youtube_url(url)
   query=f"summarize this {transcript} transcript"
-  summary = chat(query)
+  summary = summarization(query)
   return summary
 
 
-iface = gr.Interface(
-    fn=main,
-    inputs="text",
-    outputs="text",
-    
-    title="Video Özeti Oluşturucu",
-    description="Bir YouTube videosunun URL'sini girin ve özetini alın."
-)
-
-iface.launch(debug = True, share = True)
+if __name__ == "__main__":
+    print(main(url))
